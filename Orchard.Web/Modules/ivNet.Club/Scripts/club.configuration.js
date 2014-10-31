d776a0881e7d7baa8d7ce3e00f966702e2945282
bp <bp@ivnet.co.uk>
2014-10-31 18:32:20 +0000

    club config item table display

--- Orchard.Web/Modules/ivNet.Club/Scripts/club.configuration.js
+++ Orchard.Web/Modules/ivNet.Club/Scripts/club.configuration.js
@@ -1,4 +1,15 @@
-﻿var ivNetClubConfiguration = angular.module('ivNet.Club.Configuration.App', ['ngResource','trNgGrid']);
+﻿var ivNetClubConfiguration = angular.module('ivNet.Club.Configuration.App', ['ngResource', 'trNgGrid'])
+    .filter("dateField", function() {
+        return function (combinedFieldValueUnused, item) {
+            var d = item.Date;
+            
+            var curr_date = d.substr(8, 2);
+            var curr_month = d.substr(5, 2);
+            var curr_year = d.substr(0, 4);
+          
+            return curr_date + "/" + curr_month + "/" + curr_year;
+        };
+});
 
 ivNetClubConfiguration.factory("configFactory", function ($resource) {
     return $resource("/api/club/configuration");
@@ -13,4 +24,8 @@ ivNetClubConfiguration.controller('ConfigurationController', function ($scope, c
     //configFactory.get({ id: 1 }, function (data) {
     //    $scope.post = data;
     //});
+
+    $scope.editItem = function(item) {
+        alert(item.Id);
+    };
 });
